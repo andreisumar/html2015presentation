1550d03c88f878e1df4416fd43e72eb7a0cb5bd2
Vladimir.Zhidal <vzhidal@mercle.com>
2015-07-06 23:19:18 +0300

    Updated formatting. Added html5 semantic tags. Added notes for most of slides.

--- examples/json-generate.js
+++ examples/json-generate.js
@@ -2,22 +2,22 @@ var fs = require('fs'),
     path = require('path');
 
 function getDirectories(srcpath) {
-  return fs.readdirSync(srcpath).filter(function(file) {
-    return fs.statSync(path.join(srcpath, file)).isDirectory();
-  });
+    return fs.readdirSync(srcpath).filter(function(file) {
+        return fs.statSync(path.join(srcpath, file)).isDirectory();
+    });
 }
 
 var dirs = getDirectories('.');
 
 var result = [];
 
-for (var i=0; i<dirs.length; ++i) {
-	var titleFileSrc = '.' + '/' + dirs[i] + '/title.txt';
-	var title = fs.readFileSync(titleFileSrc, 'utf8');
-	result.push({
-		id: dirs[i],
-		title: title
-	});
+for(var i = 0; i < dirs.length; ++i) {
+    var titleFileSrc = '.' + '/' + dirs[i] + '/title.txt';
+    var title = fs.readFileSync(titleFileSrc, 'utf8');
+    result.push({
+        id: dirs[i],
+        title: title
+    });
 }
 
 console.log(JSON.stringify(result, true, 4));
